0e17802a65f2a8d674e1e6b6761465563a8e4b19
himanushi <himanushi@users.noreply.github.com>
2026-01-11 23:02:34 +0900

    fix: main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");          // 数を当ててごらん
+
+    println!("Please input your guess.");   // ほら、予想を入力してね
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("aaa");
+
+    println!("You guessed: {}", guess);     // 次のように予想しました: {}
 }
