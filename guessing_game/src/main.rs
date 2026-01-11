27561669d37a713df74f1f550100355106d9e44a
himanushi <himanushi@users.noreply.github.com>
2026-01-11 23:54:10 +0900

    fix: main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");          // 数を当ててごらん
 
-    println!("Please input your guess.");   // ほら、予想を入力してね
+    let secret_number = rand::thread_rng().gen_range(1..10);
+
+    println!("The secret number is: {}", secret_number);   // ほら、予想を入力してね
 
     let mut guess = String::new();
 
