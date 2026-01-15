5bd3f47f66ca6e13e4cb97931c53a1949d13d7f3
himanushi <himanushi@users.noreply.github.com>
2026-01-15 19:32:42 +0900

    fix: main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,14 +1,5 @@
 fn main() {
-    let x = 5;
+    let x = 2.0; // f64
 
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-
-    println!("The value of x is: {}", x);
-
-    let guess: u32 = "42".parse().expect("Not a number!");
+    let y: f32 = 3.0; // f32
 }
