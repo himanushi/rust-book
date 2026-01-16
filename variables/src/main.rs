4620d0525068088e814dfef8408603b3969e865d
himanushi <himanushi@users.noreply.github.com>
2026-01-16 23:25:01 +0900

    fix: main.rs

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,15 +1,9 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    println!("hello");
 
-    println!("Please enter an array index.");
+    another_function();
+}
 
-    let mut index = String::new();
-
-    std::io::stdin().read_line(&mut index).expect("error1");
-
-    let index: usize = index.trim().parse().expect("error2");
-
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+fn another_function() {
+    println!("another");
 }
