227a692164a32a80a8d2ab5528b796586d3a52c5
himanushi <himanushi@users.noreply.github.com>
2026-01-21 22:33:01 +0900

    fix: main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+fn main() {
+    let s = String::from("test aaa");
+    let len = first_word(s);
+    println!("{len}")
+}
+
+fn first_word(s: String) -> usize {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
