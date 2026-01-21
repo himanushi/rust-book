37dd4d0b58211ea8ff370b90d743cbe429109451
himanushi <himanushi@users.noreply.github.com>
2026-01-21 22:54:23 +0900

    fix: main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
     let mut s = String::from("test aaa");
     let word = first_word(&s);
-    s.clear();
+    // s.clear();
     println!("{word}");
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
