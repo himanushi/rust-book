7b2b8cfed245dae6f9367a868df9c728b5d03c2a
himanushi <himanushi@users.noreply.github.com>
2026-01-21 10:01:15 +0900

    fix: main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut s = String::from("hello");
+    let r2 = &mut s;
 
     {
         let r1 = &mut s;
-    } // r1はここでスコープを抜けるので、問題なく新しい参照を作ることができる
 
-    let r2 = &mut s;
+        println!("{}, {}", r1, r2);
+    } // r1はここでスコープを抜けるので、問題なく新しい参照を作ることができる
 }
