3a8805121e9eb189a0e6f83822dca74450ed3950
himanushi <himanushi@users.noreply.github.com>
2026-01-21 10:00:19 +0900

    fix: main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     let mut s = String::from("hello");
 
+    let r1 = &mut s;
+
+    let mut s2 = String::from("hello");
+    let r2 = &mut s2;
+
+    println!("{}, {}", r1, r2);
+
     change(&mut s);
 }
 
