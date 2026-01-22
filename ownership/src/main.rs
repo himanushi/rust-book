4559a4c7743113c8fe263c3602f25f27c247753b
himanushi <himanushi@users.noreply.github.com>
2026-01-22 22:24:36 +0900

    fix: main.rs

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,21 @@
 fn main() {
-    let mut s = String::from("test aaa");
-    let word = first_word(&s);
-    // s.clear();
-    println!("{word}");
+    let my_string = String::from("hello world");
+
+    // first_word works on slices of `String`s
+    // first_wordは`String`のスライスに対して機能する
+    let word = first_word(&my_string[..]);
+
+    let my_string_literal = "hello world";
+
+    // first_word works on slices of string literals
+    // first_wordは文字列リテラルのスライスに対して機能する
+    let word = first_word(&my_string_literal[..]);
+
+    // Because string literals *are* string slices already,
+    // this works too, without the slice syntax!
+    // 文字列リテラルは「それ自体すでに文字列スライスなので」、
+    // スライス記法なしでも機能するのだ！
+    let word = first_word(my_string_literal);
 }
 
 fn first_word(s: &str) -> &str {
