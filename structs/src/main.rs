99378c4242e8c2c4643aff3ef68f20dc7a6daca3
himanushi <himanushi@users.noreply.github.com>
2026-01-23 21:07:54 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,14 +6,21 @@ struct User {
 }
 
 fn main() {
-    let mut user = User {
+    let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
 
-    user.username = String::from("someusername");
+    user1.username = String::from("someusername");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        username: String::from("anotherusername567"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
 
     println!("Hello, world!");
 }
