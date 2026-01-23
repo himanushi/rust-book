c20facd0fea83973db71de3f72867b1fd61ef72a
himanushi <himanushi@users.noreply.github.com>
2026-01-23 21:06:28 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -17,3 +17,12 @@ fn main() {
 
     println!("Hello, world!");
 }
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email: email,
+        username: username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
