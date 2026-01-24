a13a9a9dd3596518ff1be4fa692fccc914932060
himanushi <himanushi@users.noreply.github.com>
2026-01-24 23:04:17 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,5 +10,5 @@ fn main() {
         height: 50,
     };
 
-    println!("rect1 is {}", rect1);
+    println!("rect1 is {:?}", rect1);
 }
