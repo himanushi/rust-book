898f950f5fdb4982d197e39601ab015c575f413d
himanushi <himanushi@users.noreply.github.com>
2026-01-24 23:01:02 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect = (30, 50);
 
-    println!("aaa {}", area(width1, height1))
+    println!("aaa {}", area(rect))
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rect: (u32, u32)) -> u32 {
+    rect.0 * rect.1
 }
