5af013d891666050aec0f8e5a0287233a8a58a7c
himanushi <himanushi@users.noreply.github.com>
2026-01-24 22:32:13 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,12 +1,3 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
-struct Color(i32, i32, i32);
-
 fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
@@ -26,13 +17,4 @@ fn main() {
     let black = Color(0, 0, 0);
 
     println!("Hello, world!");
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
 }
