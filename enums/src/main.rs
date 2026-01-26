0d6d4c19498a567712a60f27da2ed31a1b551f55
himanushi <himanushi@users.noreply.github.com>
2026-01-26 22:25:54 +0900

    fix: main.rs

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,4 +1,10 @@
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
 fn main() {
-    let x: i8 = 5;
-    let y: Option<i8> = Some(5);
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
 }
