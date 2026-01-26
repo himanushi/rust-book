b860835985a53869a0422aea0b6015e36be03255
himanushi <himanushi@users.noreply.github.com>
2026-01-26 22:39:30 +0900

    fix: main.rs

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -17,7 +17,7 @@ fn value_in_cents(coin: Coin) -> u32 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(&state) => 25,
     }
 }
 
