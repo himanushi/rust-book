93833d908dbec2dfe520086dbd4047a8244cc50d
himanushi <himanushi@users.noreply.github.com>
2026-01-26 22:14:37 +0900

    fix: main.rs

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,10 +1,6 @@
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
-
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    let sum = x + y;
 }
