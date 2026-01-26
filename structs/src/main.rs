820eaaaded68246f0bec0eb8c12c3179f20254a2
himanushi <himanushi@users.noreply.github.com>
2026-01-26 21:48:03 +0900

    fix: main.rs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -15,6 +15,16 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {}", rect1.area());
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
