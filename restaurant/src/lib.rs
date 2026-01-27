acc08923ae1212cb60bbbe8fca2e107f7bc75f0c
himanushi <himanushi@users.noreply.github.com>
2026-01-27 22:57:34 +0900

    fix: lib.rs

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,11 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    // 絶対パス
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    // 相対パス
-    front_of_house::hosting::add_to_waitlist();
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
