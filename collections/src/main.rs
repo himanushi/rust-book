489cf6bdb34bb9abc91a85315a780a56f0daad6b
himanushi <himanushi@users.noreply.github.com>
2026-01-29 22:52:25 +0900

    fix: main.rs

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,6 +1,7 @@
+use std::ops::RangeFull;
+
 fn main() {
     let v = vec![1, 2, 3, 4, 5];
-
-    let does_not_exist = &v[100];
-    let does_not_exist = v.get(100);
+    let all = &v[RangeFull]; // ✅ 書ける！
+    println!("{:?}", all); // [1, 2, 3, 4, 5]
 }
