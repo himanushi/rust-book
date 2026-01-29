870b3b9d8cc35c4f4a90fa7506b56b4e54f3305e
himanushi <himanushi@users.noreply.github.com>
2026-01-29 23:00:40 +0900

    fix: main.rs

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5];
-
-    let first = &v[0];
-
-    println!("The first element is: {}", first);
-
-    v.push(6);
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
 }
