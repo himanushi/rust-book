a8f9a63391fe4f90b4f49e207bf57170d3eeb3f4
himanushi <himanushi@users.noreply.github.com>
2026-02-01 17:42:32 +0900

    fix: main.rs

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,11 +1,11 @@
 use std::collections::HashMap;
 
 fn main() {
-    let field_name = String::from("Favorite color");
-    let field_value = String::from("Blue");
+    let mut scores = HashMap::new();
 
-    let mut map = HashMap::new();
-    map.insert(&field_name, field_value);
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    println!("{field_name}")
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
 }
