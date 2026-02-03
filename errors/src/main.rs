e0cf8cd6d7eead03c785ac3ef092f0578d8588d7
himanushi <himanushi@users.noreply.github.com>
2026-02-03 22:33:55 +0900

    fix: main.rs

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -2,4 +2,12 @@ use std::fs::File;
 
 fn main() {
     let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            // ファイルを開く際に問題がありました
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    };
 }
