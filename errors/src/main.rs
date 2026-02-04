8db406d7115d34db7afdf3721b2a9da7dc23b0e3
himanushi <himanushi@users.noreply.github.com>
2026-02-04 22:13:39 +0900

    fix: main.rs

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,12 +1,24 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => {
+            match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!(
+                        //ファイルを作成しようとしましたが、問題がありました
+                        "Tried to create file but there was a problem: {:?}",
+                        e
+                    )
+                }
+            }
+        }
         Err(error) => {
-            // ファイルを開く際に問題がありました
             panic!("There was a problem opening the file: {:?}", error)
         }
     };
