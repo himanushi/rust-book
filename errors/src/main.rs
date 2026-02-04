e4f74e470c02380b823d93f21bd46ba1e9b10adc
himanushi <himanushi@users.noreply.github.com>
2026-02-04 23:00:49 +0900

    fix: main.rs

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,5 +1,26 @@
 use std::fs::File;
 
 fn main() {
-    let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let f = read_username_from_file().unwrap();
+
+    println!("{f}")
+}
+
+use std::io;
+use std::io::Read;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
