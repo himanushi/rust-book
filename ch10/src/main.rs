51122b1d2c8f8e8cdad3d2617f2547940dab7b93
himanushi <himanushi@users.noreply.github.com>
2026-02-14 10:49:55 +0900

    fix: main.rs

--- ch10/src/main.rs
+++ ch10/src/main.rs
@@ -1,84 +1,23 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
-}
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y,
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
+
+    largest
 }
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    println!("1 new tweet: {}", tweet.summarize());
-}
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-pub trait Summary {
-    fn summarize_author(&self) -> String;
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    fn summarize(&self) -> String {
-        // "（{}さんの文章をもっと読む）"
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-// impl Summary for NewsArticle {
-//     fn summarize(&self) -> String {
-//         format!("{}, by {} ({})", self.headline, self.author, self.location)
-//     }
-// }
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
-}
-
-pub fn notify(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-fn returns_summarizable(switch: bool) -> impl Summary {
-    if switch {
-        NewsArticle {
-            headline: String::from("Penguins win the Stanley Cup Championship!"),
-            location: String::from("Pittsburgh, PA, USA"),
-            author: String::from("Iceburgh"),
-            content: String::from(
-                "The Pittsburgh Penguins once again are the best \
-                 hockey team in the NHL.",
-            ),
-        }
-    } else {
-        Tweet {
-            username: String::from("horse_ebooks"),
-            content: String::from("of course, as you probably already know, people"),
-            reply: false,
-            retweet: false,
-        }
-    }
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
