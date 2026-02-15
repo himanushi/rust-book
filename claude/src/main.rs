7245c234fc7d138f6f73520f56743adcf8440bbc
himanushi <himanushi@users.noreply.github.com>
2026-02-15 23:33:53 +0900

    fix: main.rs

--- claude/src/main.rs
+++ claude/src/main.rs
@@ -1,3 +1,22 @@
+trait Animal {
+    fn name(&self) -> &str;
+}
+
+struct Dog;
+impl Animal for Dog {
+    fn name(&self) -> &str {
+        "ワンワン！"
+    }
+}
+
+struct Cat;
+impl Animal for Cat {
+    fn name(&self) -> &str {
+        "ニャー"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("ポチの自己紹介: {}", Dog.name());
+    println!("タマの自己紹介: {}", Cat.name());
 }
